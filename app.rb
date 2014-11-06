b37bcfb3edc2b83e792d4e505ac5902c5cf66eb6
David Johnson <davidjohnson1297@gmail.com>
2014-11-05 19:09:46 -0800

    fin

---
+++ app.rb
@@ -0,0 +1,26 @@
+require 'sinatra'
+
+set :static, true 
+
+bands = ['Nine Inch Nails', 'Justin Beiber', 'Katy Perry', 'Spoon', 'Radiohead', 'Elvis']
+venues = ["Hollywood Bowl", "The Greek", "Gramma's Livingroom", "Staples", "The Roxy"]
+seats = ["VIP", "GA", "PL (parking lot)"]
+
+
+get '/' do
+	@bands = bands
+	@venues = venues
+	@seats = seats
+	erb :home
+end
+
+get '/results' do
+	band  = params['bands']
+	venue = params['venues']
+	seat  = params['seats']
+	@band = band
+	@venues = venue
+	@seats = seat
+	erb :results
+end
+
